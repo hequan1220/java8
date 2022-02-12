5d5ed970f1e050d614c13aaae07c0fc949d00757
hequan <04140824Hq>
2022-02-12 16:53:47 +0800

    first commit

--- src/main/java/com/example/java8/Java8Application.java
+++ src/main/java/com/example/java8/Java8Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Java8Application {
-        //
+        //www
     public static void main(String[] args) {
         SpringApplication.run(Java8Application.class, args);
     }
